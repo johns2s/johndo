5192bd70912335b9da28bfd66798b64740dd305e
johns2s <johns2ses@zoho.com>
2018-08-22 17:42:22 -0400

    initial design

--- newtask.php
+++ newtask.php
@@ -30,8 +30,26 @@
       ?>
       <label class = "formLabel">Task Name</label>
       <input type = "text" class = "inputText" name = "titleInput" maxlength = "75" placeholder = "Task Title" value = "<?php echo htmlentities($taskTitle); ?>" required>
+
+      <label class = "formLabel">Recurring?</label>
+      <div class = "radioWrap">
+        <input id="once" style="display: none" name="recurring" value="once" type="radio">
+        <input id="repeat" style="display: none" name="recurring" value="repeat" type="radio">
+        <label for="once" id = "onceLabel" class="radioLabel">One Time</label>
+        <label for="repeat" id = "repeatLabel" class="radioLabel">Repeat</label>
+      </div>
+
       <label class = "formLabel">Deadline</label>
       <input type = "date" class = "inputText" name = "dateInput" minlength = "5" maxlength = "10" placeholder = "Deadline (MM/DD/YYYY)" required>
+
+      <label class = "formLabel">Frequency (days)</label>
+      <input type = "number" class = "inputText" name = "freqInput" min = "1" max = "20" placeholder = "Repeat every x days" required>
+      <label class = "formLabel">Start date</label>
+      <input type = "date" class = "inputText" name = "startInput" placeholder = "Start date (MM/DD/YYYY)" required>
+      <label class = "formLabel">End date</label>
+      <input type = "date" class = "inputText" name = "endInput" placeholder = "End date (MM/DD/YYYY)" required>
+
+
       <label class = "formLabel">More Info</label>
       <textarea class = "inputTextBig" name = "moreInput"  placeholder = "More Info"></textarea>
       <input type = "submit" class = "inputBtn" name = "submitNew" value = "Add" required>
